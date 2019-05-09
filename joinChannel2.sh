e7ab0a3c921556559b1ec6ab5105cfd832bc8f55
dyahayu <dyahayu@pc2.local>
2019-05-09 02:49:37 +0000

    ini udah berhasil

--- joinChannel2.sh
+++ joinChannel2.sh
@@ -1,9 +1,9 @@
 # Create the channel
-docker exec peer0.org1.hospital.com peer channel create -o orderer.hospital.com:7050 -c composerchannel -f /var/hyperledger/channel-artifacts/channel.tx --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
+docker exec peer0.org1.hospital.com peer channel create -o orderer.hospital.com:7050 -c rschannel -f /var/hyperledger/channel-artifacts/channel.tx --tls true --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
 
 # Join peer0.org1.example.com to the channel.
-docker exec peer0.org1.hospital.com peer channel join -b composerchannel.block
+docker exec peer0.org1.hospital.com peer channel join -b rschannel.block
 
 # Join peer1.org1.example.com to the channel.
-docker exec peer1.org1.hospital.com peer channel fetch config -o orderer.hospital.com:7050 -c composerchannel --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
-docker exec peer1.org1.hospital.com peer channel join -b composerchannel_config.block
+docker exec peer1.org1.hospital.com peer channel fetch config -o orderer.hospital.com:7050 -c rschannel --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
+docker exec peer1.org1.hospital.com peer channel join -b rschannel_config.block
