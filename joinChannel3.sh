81e73086bf019c86a0df2138b2fb3534c1c14002
Your Name <you@example.com>
2019-05-10 13:29:48 +0700

    ini suhh

--- joinChannel3.sh
+++ joinChannel3.sh
@@ -1,8 +1,8 @@
 
 # Join peer0.org1.example.com to the channel.
-docker exec peer0.org2.hospital.com peer channel fetch config -o orderer.hospital.com:7050 -c composerchannel --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
-docker exec peer0.org2.hospital.com peer channel join -b composerchannel_config.block
+docker exec peer0.org2.hospital.com peer channel fetch config -o orderer.hospital.com:7050 -c rschannel --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
+docker exec peer0.org2.hospital.com peer channel join -b rschannel_config.block
 
 # Join peer1.org1.example.com to the channel.
-docker exec peer1.org2.hospital.com peer channel fetch config -o orderer.hospital.com:7050 -c composerchannel --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
-docker exec peer1.org2.hospital.com peer channel join -b composerchannel_config.block
+docker exec peer1.org2.hospital.com peer channel fetch config -o orderer.hospital.com:7050 -c rschannel --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/hospital.com/msp/tlscacerts/tlsca.hospital.com-cert.pem
+docker exec peer1.org2.hospital.com peer channel join -b rschannel_config.block
